10fa36fc7484fb72da8a1a43583b9680e8175a61
jyooni <berry3891@naver.com>
2024-11-08 00:04:13 +0900

    section09: React 최적화 (useCallback, useMemo, React.memo)

---
+++ section09/src/components/TodoItem.jsx
@@ -0,0 +1,28 @@
+import React, { memo } from "react";
+import "./TodoItem.css";
+
+function TodoItem({ todo, onChecked, onDelete }) {
+  return (
+    <div className="TodoItem">
+      <input
+        checked={todo.isDone}
+        onChange={() => onChecked(todo.id)}
+        type="checkbox"
+      />
+      <div className="content">{todo.content}</div>
+      <div className="date">{new Date(todo.date).toLocaleDateString()}</div>
+      <button onClick={() => onDelete(todo.id)}>삭제</button>
+    </div>
+  );
+}
+
+// export default memo(TodoItem, (prevProps, nextProps) => {
+//   if (prevProps.todo.id !== nextProps.todo.id) return false;
+//   if (prevProps.todo.isDone !== nextProps.todo.isDone) return false;
+//   if (prevProps.todo.content !== nextProps.todo.content) return false;
+//   if (prevProps.todo.date !== nextProps.todo.date) return false;
+
+//   return true;
+// });
+
+export default memo(TodoItem);
