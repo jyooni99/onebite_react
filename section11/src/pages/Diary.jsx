94e3796a133fa236abeb233c9fae69161a4cf68d
jyooni <berry3891@naver.com>
2024-11-18 17:56:43 +0900

    section11: favicon, title, open graph 설정

--- section11/src/pages/Diary.jsx
+++ section11/src/pages/Diary.jsx
@@ -4,11 +4,13 @@ import Button from "../components/Button";
 import Viewer from "../components/Viewer";
 import { useDiary } from "../hooks/useDiary";
 import { getStringedDate } from "../util/get-stringed-date";
+import usePageTitle from "../hooks/usePageTitle";
 
 const Diary = () => {
   const { id } = useParams();
   const nav = useNavigate();
   const diaryItem = useDiary(id);
+  usePageTitle(`${id}번 일기`);
 
   if (!diaryItem) {
     return <div>데이터 로딩중 ...</div>;
--- section11/src/pages/New.jsx
+++ section11/src/pages/New.jsx
@@ -4,10 +4,11 @@ import Button from "../components/Button";
 import Editor from "../components/Editor";
 import { useNavigate } from "react-router-dom";
 import { DiaryDispatchContext } from "../App";
-
+import usePageTitle from "../hooks/usePageTitle";
 const New = () => {
   const { onCreate } = useContext(DiaryDispatchContext);
   const nav = useNavigate();
+  usePageTitle("새 일기 쓰기");
 
   const onSubmit = (input) => {
     onCreate(
